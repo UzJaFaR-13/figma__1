5132fbccc528dfcea6c102e0114ed08b9484f9a4
UzJaFaR-13 <88071148+UzJaFaR-13@users.noreply.github.com>
2021-09-12 03:05:15 +0500

    slider edit
    slider edit

--- js/main.js
+++ js/main.js
@@ -160,7 +160,6 @@ facebook2.addEventListener("click", function() {
 });
 
 // Slides===============
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -175,15 +174,35 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot")
-    if (n > slides.length) { slideIndex = }
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
+        slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.repeat("active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active"
+    dots[slideIndex - 1].className += " active";
 }
+
+
+// Auto Slide   if you need auto slide ,remove the commit "//"
+
+
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var i;
+//    var slides = document.getElementsByClassName("mySlides");
+//    for (i = 0; i < slides.length; i++) {
+//        slides[i].style.display = "none";
+//    }
+//    slideIndex++;
+//    if (slideIndex > slides.length) { slideIndex = 1 }
+//    slides[slideIndex - 1].style.display = "block";
+//    setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
