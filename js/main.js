1eb50b0eaf010b858f91f419aef75e6821a844d3
UzJaFaR-13 <88071148+UzJaFaR-13@users.noreply.github.com>
2021-09-11 09:27:53 +0300

    silader
    slader

--- js/main.js
+++ js/main.js
@@ -158,3 +158,32 @@ facebook2.addEventListener("click", function() {
     headerblock.style.border = "none"
 
 });
+
+// Slides===============
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot")
+    if (n > slides.length) { slideIndex = }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.repeat("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active"
+}
